f3d07fca27577b1c73de1de2cb4c4d5c5d6582f2
SausageMuffins <rwang9098@gmail.com>
2023-07-11 19:26:49 +0800

    added extra error handling

--- lambda_functions/index.js
+++ lambda_functions/index.js
@@ -10,15 +10,25 @@ const pool = new Pool({
     port: 5432,
 });
 
-exports.handler = async (event) => {
+exports.handler = async (event, context) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    
     // Parse the input body
     const { num1, num2 } = JSON.parse(event.body);
 
     // Perform the multiplication
     const result = num1 * num2;
 
-    // Save the result in the PostgreSQL database
-    await pool.query('INSERT INTO results(value) VALUES($1)', [result]);
+    try {
+        // Save the result in the PostgreSQL database
+        await pool.query('INSERT INTO results(value) VALUES($1)', [result]);
+    } catch (error) {
+        console.error(`Error while inserting into DB: ${error}`);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to insert into DB' }),
+        };
+    }
 
     // Return the result
     return {
