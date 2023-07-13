cf5ddd970c861381d831751c14f348fbbbd1e0f3
SausageMuffins <rwang9098@gmail.com>
2023-07-13 18:22:35 +0800

    added configuration to index.js

--- src/index.js
+++ src/index.js
@@ -14,6 +14,19 @@ root.render(
   </React.StrictMode>
 );
 
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "multiplier", // you'll use this name to send requests to the API
+        endpoint: "https://4b39guzzz0.execute-api.ap-southeast-1.amazonaws.com/dev", // replace with your actual API Gateway endpoint
+        region: "ap-southeast-1" // replace with your actual region
+      }
+    ]
+  }
+});
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
