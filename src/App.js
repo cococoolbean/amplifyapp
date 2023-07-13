177ecfe952cb48989e8f51c7cad526ee2a0fbaf4
cococoolbean <cococoolbeana@gmail.com>
2023-07-13 18:57:58 +0800

    remove config

--- src/App.js
+++ src/App.js
@@ -12,24 +12,6 @@ import {
 } from "@aws-amplify/ui-react";
 import { Auth } from 'aws-amplify';
 import { API } from 'aws-amplify';
-Amplify.configure({
-  //Auth is the same as before
-  Auth: {
-    region: "us-east-1",
-    userPoolId: "us-east-1_T2ZfRqx59",
-    userPoolWebClientId: "61l5lp494qnb60ek6h0ohonspp",
-  },
-  // Add in our new API, "name" can be whatever we want
-  API: {
-    endpoints: [
-      {
-        name: "multiplier",
-        endpoint:
-          "https://4b39guzzz0.execute-api.ap-southeast-1.amazonaws.com/dev",
-      },
-    ],
-  },
-});
 function App({ signOut }) {
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
