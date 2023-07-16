a7e53f2ed55558d2a7ea687377f6764d429a6fe9
cococoolbean <cococoolbeana@gmail.com>
2023-07-16 20:14:00 +0800

    feat: update response

--- amplify/backend/function/dividelambda/src/index.js
+++ amplify/backend/function/dividelambda/src/index.js
@@ -42,11 +42,6 @@ const handler = async (event) => {
 
         const response = {
             statusCode: 200,
-            headers:{
-              "Access-Control-Allow-Headers" : 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
-              "Access-Control-Allow-Origin": '*',
-              "Access-Control-Allow-Methods": '*'
-            },
             body: JSON.stringify({ 'result': result }),
         };
 
